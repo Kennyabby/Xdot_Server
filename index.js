47f593782833e1e2fa2b8e28a41986ad29c84c75
kennyabby <kennyabby22@gmail.com>
2022-07-13 09:34:43 +0100

    allowed headers

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ var delivered = false
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
-
+app.use((req, res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.post('/postUserDetails', async (req, res) => {
   const user = await req.body.info
   const password = user.password
