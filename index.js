8a6c7b1df281d15cde964b2298898be2be04e77c
kennyabby <kennyabby22@gmail.com>
2022-07-13 18:45:12 +0100

    added file limit

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ var propList = []
 var array = {}
 var updated = false
 var delivered = false
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(cors())
 app.use(bodyParser.json())
 app.use((req, res, next)=>{
