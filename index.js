6ecdeb94997bf3f931c56f75a2406cdd9641c42c
kennyabby <kennyabby22@gmail.com>
2023-02-20 14:12:11 +0100

    server update

--- index.js
+++ index.js
@@ -410,7 +410,6 @@ app.post('/closeSession', async (req, res) => {
     })
 })
 app.post('/getpassList', async (req, res) => {
-  // Updating this.
   await main(
     (func = 'findOne'),
     (database = 'naps'),
